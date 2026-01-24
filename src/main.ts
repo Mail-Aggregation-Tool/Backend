dc97c63600dcbc6621e253af7f8a11d443f9f0f1
iamkhalidll <youngatere04@mgmail.com>
2026-01-25 00:49:55 +0100

    feat: Bootstrap NestJS application with global validation, CORS, and Swagger API documentation.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
+  app.enableCors({
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000'], 
+    methods: '*',
+    allowedHeaders: '*', 
+    credentials: true,   
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Mail Aggregator API')
