237f3fb99c45a92a3615586754911302baa96cfa
iamkhalidll <youngatere04@mgmail.com>
2026-01-29 21:55:17 +0100

    fixed cookie parser error

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
   }));
 
-app.enableCors({
-  origin: [process.env.CLIENT_URL, 'http://localhost:3000', 'https://mat-amber.vercel.app'], 
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,   
-});
+  app.enableCors({
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000', 'https://mat-amber.vercel.app'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Email Aggregator API')
